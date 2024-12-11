ade51e9a2bc5696882dcd4df26a7e63b9ae3be11
andrii <andrii.dovzhenko.dv@gmail.com>
2024-12-11 22:33:11 +0200

    Update graphs

--- clients/python3/display.py
+++ clients/python3/display.py
@@ -2,17 +2,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-threads = [1, 2, 5, 10, 100, 1000, 10000]
-thread_creation = [0.000401, 0.000381, 0.000383, 0.000318, 0.000319, 0.000401, 0.000578]
-document_loading = [0.950944, 1.020256, 1.282666, 1.531044, 1.897386, 4.041009, 6.032099]
-index_update = [116.594275, 60.42831, 31.239057, 18.827697, 23.076418, 16.464164, 12.517071]
+threads = [1, 2, 5, 10, 100, 1000]
+document_loading = [0.119457, 0.123407, 0.133936, 0.148583, 0.178151, 0.66946]
+index_update = [10.056386, 5.201664, 2.203428, 1.516104, 1.449091, 1.133619]
 
 plt.figure(figsize=(12, 6))
 width = 0.2
 
 x = np.arange(len(threads))
 
-plt.bar(x - width, thread_creation, width, label="Thread Creation (s)", color="blue")
 plt.bar(x, document_loading, width, label="Document Loading (s)", color="green")
 plt.bar(x + width, index_update, width, label="Index Update (s)", color="orange")
 
